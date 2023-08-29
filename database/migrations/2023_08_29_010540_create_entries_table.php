bb05defb630b77f5f3f8282c3cb8e9a44dc9b4be
DeveloperLewis <lewiswildmanmail@gmail.com>
2023-08-29 02:32:47 +0100

    created migrations and models for entries, habits, activities and categories

---
+++ database/migrations/2023_08_29_010540_create_entries_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('entries', function (Blueprint $table) {
+            $table->id('entry_id');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('activity_id');
+            $table->foreignId('category_id')->nullable();
+            $table->integer('time_performed');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('entries');
+    }
+};
