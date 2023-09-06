573852a1b11502b208401a9cb7083f6dba59c2b3
DeveloperLewis <lewiswildmanmail@gmail.com>
2023-09-06 18:30:00 +0100

    Made dynamic navbar to show that user is on current page

---
+++ resources/views/components/partials/dashboard_navbar.blade.php
@@ -0,0 +1,18 @@
+<div class="bg-custom-blue-light text-lg md:text-4xl flex flex-col whitespace-nowrap pt-5">
+    <a href="#" class="cursor-default pointer-events-none text-xs py-1 text-gray-300 font-black px-5 my-1 md:px-10">TIME TRACKING</a>
+    <x-ui.general.dashboard_link href="/dashboard/entries">ENTRIES</x-ui.general.dashboard_link>
+    <x-ui.general.dashboard_link href="/dashboard/habits">HABITS</x-ui.general.dashboard_link>
+
+    <a href="#" class="cursor-default pointer-events-none text-xs text-gray-300 font-black mt-5 px-5 my-1 md:px-10">ANALYTICS</a>
+    <x-ui.general.dashboard_link href="/dashboard/calendar">CALENDAR</x-ui.general.dashboard_link>
+
+    <a href="#" class="cursor-default pointer-events-none text-xs text-gray-300 font-black mt-5 px-5 my-1 md:px-10">EPOCHIUM</a>
+    <x-ui.general.dashboard_link href="/dashboard/character">CHARACTER</x-ui.general.dashboard_link>
+    <x-ui.general.dashboard_link href="/dashboard/skills">SKILLS</x-ui.general.dashboard_link>
+    <x-ui.general.dashboard_link href="/dashboard/inventory">INVENTORY</x-ui.general.dashboard_link>
+    <x-ui.general.dashboard_link href="/dashboard/shop">SHOP</x-ui.general.dashboard_link>
+
+    <a href="#" class="cursor-default pointer-events-none text-xs text-gray-300 font-black mt-5 px-5 my-1 md:px-10">OTHER</a>
+    <x-ui.general.dashboard_link href="/dashboard/account">ACCOUNT</x-ui.general.dashboard_link>
+    <x-ui.general.dashboard_link href="/dashboard/settings">SETTINGS</x-ui.general.dashboard_link>
+</div>
--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -4,24 +4,7 @@
     <x-navbar></x-navbar>
 
     <div class="flex h-screen border-t border-gray-600">
-        <div class="bg-custom-blue-light text-lg md:text-4xl flex flex-col whitespace-nowrap pt-5">
-            <a href="#" class="cursor-default pointer-events-none text-xs py-1 text-gray-300 font-black px-5 my-1 md:px-10">TIME TRACKING</a>
-            <x-ui.general.dashboard-link href="/dashboard/entries">ENTRIES</x-ui.general.dashboard-link>
-            <x-ui.general.dashboard-link href="/dashboard/habits">HABITS</x-ui.general.dashboard-link>
-
-            <a href="#" class="cursor-default pointer-events-none text-xs text-gray-300 font-black mt-5 px-5 my-1 md:px-10">ANALYTICS</a>
-            <x-ui.general.dashboard-link href="/dashboard/calendar">CALENDAR</x-ui.general.dashboard-link>
-
-            <a href="#" class="cursor-default pointer-events-none text-xs text-gray-300 font-black mt-5 px-5 my-1 md:px-10">EPOCHIUM</a>
-            <x-ui.general.dashboard-link href="/dashboard/character">CHARACTER</x-ui.general.dashboard-link>
-            <x-ui.general.dashboard-link href="/dashboard/skills">SKILLS</x-ui.general.dashboard-link>
-            <x-ui.general.dashboard-link href="/dashboard/inventory">INVENTORY</x-ui.general.dashboard-link>
-            <x-ui.general.dashboard-link href="/dashboard/shop">SHOP</x-ui.general.dashboard-link>
-
-            <a href="#" class="cursor-default pointer-events-none text-xs text-gray-300 font-black mt-5 px-5 my-1 md:px-10">OTHER</a>
-            <x-ui.general.dashboard-link href="/dashboard/account">ACCOUNT</x-ui.general.dashboard-link>
-            <x-ui.general.dashboard-link href="/dashboard/settings">SETTINGS</x-ui.general.dashboard-link>
-        </div>
+        <x-partials.dashboard_navbar></x-partials.dashboard_navbar>
 
         <div class="bg-cover bg-no-repeat w-full" style="background-image: url('{{asset('/images/dashboard_background_01.webp')}}');">
            <p class="text-4xl text-center mt-10 font-semibold">WELCOME TO THE DASHBOARD</p>
